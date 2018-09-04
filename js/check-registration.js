57540a38a8490bd7800340c4587ae3361dfe251c
Victor Sharamet <sharamet@gmail.com>
2018-09-05 01:31:22 +0300

    first commit

--- js/check-registration.js
+++ js/check-registration.js
@@ -1,5 +1,50 @@
 $(document).ready(function() {
 
-	console.log('Hello world!');
+	$('#email').on('focus', function(){
+		$('#errorEmailEmpty').slideUp();
+		$('#errorEmailFormat').slideUp();
+		$('#errorEmailRestricted').slideUp();
+	});
+	$('#password').on('focus', function(){
+		$('#errorPassEmpty').slideUp();
+	});
 
+	$('#formRegister').on('submit', function(e){
+		e.preventDefault();
+		var email = $('#email').val();
+			password = $('#password').val();
+			pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+			validEmail = false,
+			validPass = false,
+			testEmail = 'mail@mail.com';
+
+		// Email Check
+		if ( email == '' ) {
+			$('#errorEmailEmpty').slideDown();
+		} else {
+			if ( pattern.test(email) ) {
+				validEmail = true;
+			} else {
+				$('#errorEmailFormat').slideDown();
+			}
+		}
+
+		// Password check
+		if ( password == '' ) {
+			$('#errorPassEmpty').slideDown();
+		} else {
+			validPass = true;
+		}
+
+		// Test email check
+		if (validPass == true && validEmail == true) {
+			if ( email == testEmail ) {
+				$('#errorEmailRestricted .notify').show();
+				$('#errorEmailRestricted').slideDown();
+			} else {
+				$('#formRegister').unbind('submit');
+				$('#formRegister').submit();
+			}
+		}
+	})
 });
