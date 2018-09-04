96f6ef19750f4f0423834d1902f2d8259bc158a4
Victor Sharamet <sharamet@gmail.com>
2018-09-04 20:10:32 +0300

    first commit

--- js/check-comment.js
+++ js/check-comment.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
 
-	console.log('Hello world!');
+	$('#commentTextarea').on('focus', function(){
+		$('#errorEmpty').slideUp();
+	});
+	$('#commentForm').on('submit', function(e){
+		e.preventDefault();
+		var commentText = $('#commentTextarea').val();
+		// console.log("Значение commentText = '" + commentText + "'");
+
+
+		if ( commentText == '' ) {
+			console.log('Пусто');
+			$('#errorEmpty').slideDown();
+		} else {
+			console.log('Комментарий есть');
+			$('#commentForm').unbind('submit');
+			$('#commentForm').submit();
+		}
+
+
+	});
+
 
 });
